791ff082b1ea7b27754d1c8c0379c0f7e3b18957
tempankush <72229553+tempankush@users.noreply.github.com>
2020-10-02 02:29:44 +0530

    sticky file added

---
+++ sticky.js
@@ -0,0 +1,17 @@
+function createSticky () {
+
+    // <div class="stickyPad">
+    //     <div class="nav-bar">
+    //         <div class="close"></div>
+    //         <div class="minimize"></div>
+    //     </div>
+    //     <div class="textbox">
+    //         <textarea name="" id="" cols="30" rows="10"></textarea></div>
+    // </div>
+
+    // DOM
+    let textbox = createBox();
+    let textArea = document.createElement("textarea");
+    textbox.appendChild(textArea);
+
+}
